70a0f7d611eb95d6b141df4264fa1b76ae0ef3d5
This_Is_Amir <arabiamir13@gmail.com>
2016-05-13 08:09:17 -0700

    Create tst.sh

---
+++ tst.sh
@@ -0,0 +1,11 @@
+clear
+echo -e "\033[38;5;208m"
+echo -e "  __ _ _ __ ___ (_)_ __   "
+echo -e " / _  |  _   _ \| |  __|  "
+echo -e "| (_| | | | | | | | |     "
+echo -e " \__,_|_| |_| |_|_|_|     "
+echo -e "                          "
+echo -e "  ThisIsAmiR Best BOT Developer    "
+echo -e "  MY Channel & Team : @OffLiNeTeam "
+echo -e "  Me In Telegram : @This_Is_AmiR   "
+echo -e "  Messnger Bot : @This_Is_AmiRBot  "
